0ccc3a8d462b4060ca8a36776187d81fad8a36ee
Rohit-Trio <rohit.trioency@gmail.com>
2024-05-28 19:07:53 +0530

    create todo app update task using livewire and some ui chnages

--- todo-app/app/Livewire/TaskLivewire.php
+++ todo-app/app/Livewire/TaskLivewire.php
@@ -10,21 +10,64 @@ class TaskLivewire extends Component
     public $todos = [];
     public $in_progress = [];
     public $complete = [];
+    public $tasks = [];
     public $status = '';
+
+    public $isEdit = [];
 
     public function render()
     {
+        $this->tasks = Task::all();
         return view('livewire.task-livewire');
-        // return view('home');
     }
 
     public function mount() {
+        $this->tasks = Task::all();
+        $this->defaultStatus();
         $this->todos = Task::where('status','to_do')->get();
         $this->in_progress = Task::where('status','in_progress')->get();
         $this->complete = Task::where('status','complete')->get();
+
     }
 
-    public function updateStatus($id) {
-        dd($id,$this->status);
+    public function defaultStatus() {
+        if(count($this->tasks) > 0) {
+            foreach($this->tasks as $i) {
+                $this->isEdit[$i->id] = false;
+                // $this->status[$i->id] = $i->status;
+            }
+        }
+    }
+
+    public function editEmp($id) {
+        try {
+            $task = Task::findOrFail($id);
+            if( !$task) {
+                session()->flash('error','Employee Task not found');
+            } else {
+                $this->status = $task->status;
+                $this->isEdit[$id] = true;
+            }
+        } catch (\Exception $ex) {
+            session()->flash('error','Something goes wrong!!');
+        }
+    }
+
+    public function resetFields()
+    {
+        $this->status = '';
+    }
+
+    public function updateEmp($id) {
+        try {
+            Task::whereId($id)->update([
+                'status' => $this->status,
+            ]);
+            session()->flash('status','Employee Task Updated Successfully!!');
+            $this->resetFields();
+            $this->isEdit[$id] = false;
+        } catch (\Exception $ex) {
+            dd(session()->flash('status','Something goes wrong!!'));
+        }
     }
 }
--- todo-app/resources/views/home.blade.php
+++ todo-app/resources/views/home.blade.php
@@ -12,7 +12,7 @@
                 margin: 5px;
             }
         </style>
-        <h1>Welcome, {{ Auth::user()->name }}!</h1>
+        {{-- <h1>Welcome, {{ Auth::user()->name }}!</h1> --}}
         <form method="POST" action="{{ route('logout') }}">
             @csrf
             <button type="submit">Logout</button>
